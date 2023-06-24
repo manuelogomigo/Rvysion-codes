75f61947ef7581061efb59f8f04e9f2fb8ce53f5
Manuel Ogomigo <manuelogomigo@gmail.com>
2023-06-24 15:00:35 +0100

    Another change

--- lenis-scroll-webflow.js
+++ lenis-scroll-webflow.js
@@ -1,46 +1,51 @@
 "use strict"; // fix lenis in safari
 
 if (Webflow.env("editor") === undefined) {
-  const lenis = new Lenis({
-    lerp: 0.1,
-    wheelMultiplier: 0.7,
-    infinite: false,
-    gestureOrientation: "vertical",
-    normalizeWheel: false,
-    smoothTouch: false
-  });
+  let lenis = null;
 
-  function raf(time) {
-    lenis.raf(time);
+  function initializeLenis() {
+    lenis = new Lenis({
+      lerp: 0.1,
+      wheelMultiplier: 0.7,
+      infinite: false,
+      gestureOrientation: "vertical",
+      normalizeWheel: false,
+      smoothTouch: false
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
     requestAnimationFrame(raf);
   }
-  requestAnimationFrame(raf);
 
-  $("[data-lenis-start]").on("click", function () {
-    lenis.start();
-    $("body").removeClass("stop-scroll"); // Add this line to remove the "stop-scroll" class
-  });
-  $("[data-lenis-stop]").on("click", function () {
-    lenis.stop();
-    $("body").addClass("stop-scroll"); // Add this line to add the "stop-scroll" class
-  });
-  $("[data-lenis-toggle]").on("click", function () {
-    $(this).toggleClass("stop-scroll");
-    if ($(this).hasClass("stop-scroll")) {
-      lenis.stop();
-      $("body").addClass("stop-scroll"); // Add this line to add the "stop-scroll" class
-    } else {
-      lenis.start();
-      $("body").removeClass("stop-scroll"); // Add this line to remove the "stop-scroll" class
+  function startLenis() {
+    if (lenis === null) {
+      initializeLenis();
     }
-  });
-
-  function connectToScrollTrigger() {
-    lenis.on("scroll", ScrollTrigger.update);
-    gsap.ticker.add((time) => {
-      lenis.raf(time * 1000);
-    });
+    lenis.start();
+    $("body").removeClass("stop-scroll"); // Remove the "stop-scroll" class
   }
-  // Uncomment this if using GSAP ScrollTrigger
-  connectToScrollTrigger();
+
+  function stopLenis() {
+    if (lenis === null) {
+      initializeLenis();
+    }
+    lenis.stop();
+    $("body").addClass("stop-scroll"); // Add the "stop-scroll" class
+  }
+
+  function toggleLenis() {
+    if (lenis === null) {
+      initializeLenis();
+    }
+    lenis.toggle();
+    $("body").toggleClass("stop-scroll"); // Toggle the "stop-scroll" class
+  }
+
+  $("[data-lenis-start]").on("click", startLenis);
+  $("[data-lenis-stop]").on("click", stopLenis);
+  $("[data-lenis-toggle]").on("click", toggleLenis);
 }
