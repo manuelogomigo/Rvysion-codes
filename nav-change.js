acbce8031d816759440faf0b08c01a6db30681da
Manuel Ogomigo <manuelogomigo@gmail.com>
2023-08-11 16:54:54 +0100

    Another change

--- nav-change.js
+++ nav-change.js
@@ -1,43 +1,35 @@
 $(".section.dark").each(function (index) {
-  const darkSection = $(this);
-  const logo1 = darkSection.find(".logo.is-1");
-  const logo2 = darkSection.find(".logo.is-2");
-
   ScrollTrigger.create({
-    trigger: darkSection,
+    trigger: $(this),
     start: "top 10%",
     end: "bottom 10%",
     onEnter: () => {
-      logo1.show();
-      logo2.hide();
       $(".nav-link, .button, .svg-arrow").addClass("light");
+      $(".logo.is-1").css("visibility", "visible");
+      $(".logo.is-2").css("visibility", "hidden");
     },
     onEnterBack: () => {
-      logo1.show();
-      logo2.hide();
       $(".nav-link, .button, .svg-arrow").addClass("light");
+      $(".logo.is-1").css("visibility", "visible");
+      $(".logo.is-2").css("visibility", "hidden");
     }
   });
 });
 
 $(".section.light").each(function (index) {
-  const lightSection = $(this);
-  const logo1 = lightSection.find(".logo.is-1");
-  const logo2 = lightSection.find(".logo.is-2");
-
   ScrollTrigger.create({
-    trigger: lightSection,
+    trigger: $(this),
     start: "top 10%",
     end: "bottom 10%",
     onEnter: () => {
-      logo2.show();
-      logo1.hide();
       $(".nav-link, .button, .svg-arrow").removeClass("light");
+      $(".logo.is-2").css("visibility", "visible");
+      $(".logo.is-1").css("visibility", "hidden");
     },
     onEnterBack: () => {
-      logo2.show();
-      logo1.hide();
       $(".nav-link, .button, .svg-arrow").removeClass("light");
+      $(".logo.is-2").css("visibility", "visible");
+      $(".logo.is-1").css("visibility", "hidden");
     }
   });
 });
