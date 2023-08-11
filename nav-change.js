9fb6fe0092ebe3e91e3f7aac8ae4275321b9e4c5
Manuel Ogomigo <manuelogomigo@gmail.com>
2023-08-11 18:13:59 +0100

    Added black logo

--- nav-change.js
+++ nav-change.js
@@ -4,20 +4,10 @@ $(".section.dark").each(function (index) {
     start: "top 10%",
     end: "bottom 10%",
     onEnter: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").addClass("light");
-      changeLogoImage(true); // Add this line to change the logo image
+      $(".nav-link, .logo-light, .logo-dark, .button, .svg-arrow").addClass("light");
     },
     onEnterBack: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").addClass("light");
-      changeLogoImage(true); // Add this line to change the logo image
-    },
-    onLeave: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").removeClass("light");
-      changeLogoImage(false); // Add this line to change the logo image
-    },
-    onLeaveBack: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").removeClass("light");
-      changeLogoImage(false); // Add this line to change the logo image
+      $(".nav-link, .logo-light, .logo-dark, .button, .svg-arrow").addClass("light");
     }
   });
 });
@@ -28,31 +18,11 @@ $(".section.light").each(function (index) {
     start: "top 10%",
     end: "bottom 10%",
     onEnter: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").removeClass("light");
-      changeLogoImage(false); // Add this line to change the logo image
+      $(".nav-link, .logo-light, .logo-dark, .button, .svg-arrow").removeClass("light");
     },
     onEnterBack: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").removeClass("light");
-      changeLogoImage(false); // Add this line to change the logo image
-    },
-    onLeave: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").addClass("light");
-      changeLogoImage(true); // Add this line to change the logo image
-    },
-    onLeaveBack: () => {
-      $(".nav-link, .logo, .button, .svg-arrow").addClass("light");
-      changeLogoImage(true); // Add this line to change the logo image
+      $(".nav-link, .logo-light, .logo-dark, .button, .svg-arrow").removeClass("light");
     }
   });
 });
 
-// Function to change the logo image
-function changeLogoImage(isLight) {
-  console.log("changeLogoImage called with isLight:", isLight);
-  var $logo = $(".logo"); // Assuming you have the logo element as a jQuery object
-  if (isLight) {
-    $logo.attr("src", "https://uploads-ssl.webflow.com/64457c55e38bb781a90b3770/64d658084b05b3c0a45205ae_Rvysion%20Logo%20(2).svg");
-  } else {
-    $logo.attr("src", "https://uploads-ssl.webflow.com/64457c55e38bb781a90b3770/64d623188b2d5acba12e9a8e_Rvysion%20Logo%20(1).svg");
-  }
-}
