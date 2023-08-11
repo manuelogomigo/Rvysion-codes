34619a0ff2a3bc0ba2c0a3a9c9f60a7f526032f4
Manuel Ogomigo <manuelogomigo@gmail.com>
2023-08-11 17:05:54 +0100

    Removed logo change

--- nav-change.js
+++ nav-change.js
@@ -1,31 +1,28 @@
 $(".section.dark").each(function (index) {
   ScrollTrigger.create({
-    trigger: this,
+    trigger: $(this),
     start: "top 10%",
     end: "bottom 10%",
     onEnter: () => {
       $(".nav-link, .button, .svg-arrow").addClass("light");
-      $(".logo.is-1").css("display", "block");
-      $(".logo.is-2").css("display", "none");
     },
-    onLeave: () => {
-      $(".nav-link, .button, .svg-arrow").removeClass("light");
+    onEnterBack: () => {
+      $(".nav-link, .button, .svg-arrow").addClass("light");
     }
   });
 });
 
 $(".section.light").each(function (index) {
   ScrollTrigger.create({
-    trigger: this,
+    trigger: $(this),
     start: "top 10%",
     end: "bottom 10%",
     onEnter: () => {
       $(".nav-link, .button, .svg-arrow").removeClass("light");
-      $(".logo.is-2").css("display", "block");
-      $(".logo.is-1").css("display", "none");
     },
-    onLeave: () => {
+    onEnterBack: () => {
       $(".nav-link, .button, .svg-arrow").removeClass("light");
     }
   });
 });
+
